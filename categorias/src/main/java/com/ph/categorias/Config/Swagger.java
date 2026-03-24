2f0f9bf8bb8b0be2f76fff7848a09bf811eb8573
unknown <paulomalavasi44@gmail.com>
2026-03-24 19:54:03 -0300

    Entity

---
+++ categorias/src/main/java/com/ph/categorias/Config/Swagger.java
@@ -0,0 +1,7 @@
+package com.ph.categorias.Config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+}
---
+++ categorias/src/main/java/com/ph/categorias/Entity/CategoriaEntity.java
@@ -0,0 +1,42 @@
+package com.ph.categorias.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeCategoria")
+    private String nome;
+    @Column(name = "descricaoCategoria")
+    private String descricao;
+
+    public CategoriaEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
